db7705ed5c265db0e995c83792d5a08bc1f604c5
Muhammad Arslan <imarslan7061@gmail.com>
2025-06-19 00:52:31 +0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Header from "./components/header";
+
+function App() {
+  return <div className="min-h-screen bg-black text-white">
+    <Header/>
+  </div>;
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return <div>
+    <div className="w-full">
+        <h1 className="text-center text-4xl font-bold">Welcome</h1>
+    </div>
+  </div>;
+}
+
+export default Header;
